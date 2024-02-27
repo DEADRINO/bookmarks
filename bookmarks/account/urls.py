af1efd50ee73543c923f382578c70c8444c5f5af
DEADRINO <121814469+DEADRINO@users.noreply.github.com>
2024-02-27 16:26:42 +0300

    Представления смены пароля

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -9,5 +9,12 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 
+    path('password_change/',
+         auth_views.PasswordChangeView.as_view(),
+         name='password_change'),
+    path('password_change/done/',
+         auth_views.PasswordChangeDoneView.as_view(),
+         name='password_change_done'),
+
     path('', views.dashboard, name='dashboard'),
 ]
